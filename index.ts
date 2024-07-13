0b7cc3c0a4007fce6862fe862b7002c30a657272
william muchui <williammuchui584@gmail.com>
2024-07-13 11:35:13 +0300

    Added delete, update methods

--- index.ts
+++ index.ts
@@ -42,8 +42,31 @@ app.post("/customer", (req: any, res: any):any =>{
     const query = "INSERT INTO CUSTOMERS(NAME, AGE, SALARY, CONTACT, ADDRESS) VALUES (?, ?, ?, ?, ?)";
     db.query(query, [NAME, AGE, SALARY, CONTACT, ADDRESS], (err:any, result:any):any =>{
         if(err) return res.status(500).json({message: "Internal server error", err: err});
+        if(result.affectedRows === 0) return res.status(404).json({message: "Customer not created"});
         return res.status(201).json({message: "Customer created successfully"});
     });
 });
+
+app.post("/update/:id", (req: any, res:any):any =>{
+    const {id} = req.params;
+    const {NAME, AGE, SALARY, CONTACT, ADDRESS} = req.body;
+    const query = "UPDATE CUSTOMERS SET NAME = ?, AGE = ?, SALARY = ?, CONTACT = ?, ADDRESS = ? WHERE ID = ?";
+    db.query(query, [NAME, AGE, SALARY, CONTACT, ADDRESS, id], (err:any, result:any):any =>{
+        if (err) return res.status(500).json({message: "Customer not found", err: err});
+        if(result.affectedRows === 0) return res.status(404).json({message: "Customer not found"});
+        return res.status(200).json({message: "Customer updated successfully"});
+    });
+});
+
+app.delete("/delete/:id", (req: any, res: any): any =>{
+    const {id} = req.params;
+    const query = "DELETE FROM CUSTOMERS WHERE ID = ?";
+    db.query(query, [id], (err:any, result:any): any =>{
+        if(err) return res.status(500).json({message: "Internal server error", err:err});
+        if(result.affectedRows === 0) return res.status(404).json({message: "Customer not found"});
+        return res.status(200).json({message: "Customer deleted successfully"});
+    });
+});
+
 
 app.listen(PORT, ()=> console.log("Server listening at port 3000"));
